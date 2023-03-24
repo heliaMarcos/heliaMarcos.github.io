21a58feb7a9ab67dbc96c1f2ec8cdf1e5bc70aad
Hélia Marcos <helia.marcos@gmail.com>
2023-03-24 23:33:49 +0000

    paths mainjs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+import './assets/styles/fonts.css';
+import './assets/styles/main.css';
+import './assets/styles/tailwind.css';
+
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import '@/assets/styles/fonts.css';
-import '@/assets/styles/main.css';
-import '@/assets/styles/tailwind.css';
-import App from '@/app.vue';
-import { routes } from '@/routes.js';
+
+import App from './app.vue';
+import { routes } from './routes.js';
+
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
