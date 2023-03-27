fec1134a4829f1b102ec90e8c30921fbe1fb8cee
Hélia Marcos <helia.marcos@gmail.com>
2023-03-27 21:20:22 +0100

    mainjs paths

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import "./assets/css/main.css";
+import "../assets/css/main.css";
 
-import { createApp } from "./../node_modules/vue/dist/vue.esm-bundler.js";
-import { createPinia } from "./../node_modules/pinia";
+import { createApp } from "../../node_modules/vue/dist/vue.esm-bundler.js";
+import { createPinia } from "../../node_modules/pinia";
 
 import gsap from './../node_modules/gsap';
-import { ScrollTrigger } from './../node_modules/gsap/ScrollTrigger';
-import { ScrollToPlugin } from './../node_modules/gsap/ScrollToPlugin';
+import { ScrollTrigger } from '../../node_modules/gsap/ScrollTrigger';
+import { ScrollToPlugin } from '../../node_modules/gsap/ScrollToPlugin';
 
 import router from "./router";
 import App from "./app.vue";
