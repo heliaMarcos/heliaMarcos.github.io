779bb88913413085fcdb577542b3cdfe13e55a6b
Hélia Marcos <helia.marcos@gmail.com>
2023-03-27 20:01:49 +0100

    workflow

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
 import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-import svgLoader from "vite-svg-loader";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), svgLoader()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+  return defineConfig({
+    base: process.env.VITE_BASE_PUBLIC_PATH,
+    server: {
+      hmr: {
+        overlay: true
+      }
     },
-  },
-});
+    plugins: [
+      svgLoader(),
+      vue()
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    }
+  });
+};
