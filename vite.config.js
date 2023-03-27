6ae340b9e4627f91be0bb474ffa55d2d59d307f7
Hélia Marcos <helia.marcos@gmail.com>
2023-03-27 20:29:12 +0100

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import svgLoader from 'vite-svg-loader';
-import { resolve } from 'path';
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    base: process.env.VITE_BASE_PUBLIC_PATH,
-    server: {
-      hmr: {
-        overlay: true
-      }
-    },
+export default defineConfig({
     plugins: [
-      svgLoader(),
-      vue()
+      vue() 
     ],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     }
-  });
-};
+});
