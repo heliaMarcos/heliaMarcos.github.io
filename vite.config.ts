ff43b9afc4a75f6e07c912232c07d905b6d8e6a5
Hélia Marcos <helia.marcos@gmail.com>
2023-03-29 02:10:21 +0100

    vite extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path'
-const pathSrc = path.resolve(__dirname, "./src");
+import { join } from 'path';
 
 export default defineConfig({
     plugins: [vue()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, './src'),
+        "@": join(__dirname, './src'),
       }
     }
 });
